a8d6dc91ec39b98187dadbcbd8e037d28072bcab
İsa Murat <isamurat233@gmail.com>
2024-01-15 22:19:54 +0300

    fix: models.py update

--- apps/users/models.py
+++ apps/users/models.py
@@ -12,11 +12,7 @@ USER_TYPE ={
 
 class Users(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    password = models.CharField()
     birth_day = models.DateField()
-    email = models.EmailField()
     phone_number = models.CharField(validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Telefon numarsını doğru girin.")],max_length=11, unique=True)
     user_type = models.CharField(choices=USER_TYPE)
     is_staff = None
@@ -26,14 +22,12 @@ class Users(AbstractUser):
     last_login = None
     user_permissions = None
     groups = None
-    class Meta:
-        verbose_name = "BaseUser"
-        verbose_name_plural = "BaseUsers"
+
 
 class Teachers(models.Model):
     teachers_profile = models.OneToOneField(Users, on_delete=models.SET_NULL, null=True)
     student = models.ForeignKey('Students', on_delete=models.SET_NULL, null=True, related_name='teacher_student')
 
 class Students(models.Model):
-    student_profile = models.OneToOneField(Users, on_delete=models.CASCADE)
+    students_profile = models.OneToOneField(Users, on_delete=models.CASCADE)
     teacher = models.OneToOneField('Teachers', on_delete=models.CASCADE, null=True, unique=True)
