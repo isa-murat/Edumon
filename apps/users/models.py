16480795230f5c55d3626e19a5b6150ceb820713
İsa Murat <isamurat233@gmail.com>
2024-01-18 00:43:32 +0300

    Update: users/models.py and users/signals.py

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,26 +1,12 @@
 import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
 
-USER_TYPE ={
-    "teacher":"Teacher",
-    "student":"Student"
-}
-
 class Users(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    birth_day = models.DateField()
-    phone_number = models.CharField(validators=[RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Telefon numarsını doğru girin.")],max_length=11, unique=True)
-    user_type = models.CharField(choices=USER_TYPE)
-
-
-class Teachers(models.Model):
-    teachers_profile = models.OneToOneField(Users, on_delete=models.SET_NULL, null=True)
-    student = models.ForeignKey('Students', on_delete=models.SET_NULL, null=True, related_name='teacher_student')
+    email = models.EmailField(unique=False,null=False, blank=False)
 
 class Students(models.Model):
     students_profile = models.OneToOneField(Users, on_delete=models.CASCADE)
-    teacher = models.OneToOneField('Teachers', on_delete=models.CASCADE, null=True, unique=True)
--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,14 +1,11 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from apps.users.models import Users, Teachers, Students
+from apps.users.models import Users, Students
 
 
 @receiver(post_save, sender=Users)
 def create_user(sender, instance, created, **kwargs):
 
     if created:
-        if instance.user_type == 'teacher':
-            Teachers.objects.create(teachers_profile=instance)
-        elif instance.user_type == 'student':
-            Students.objects.create(students_profile=instance)
+        Students.objects.create(students_profile=instance)
